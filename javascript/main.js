47981a1a563ae59c28d7ca4357c0d48936a7719f
Rohan Orton <rohan.orton@gmail.com>
2016-04-01 22:19:47 +0100

    Dropdown nav

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,17 @@
 $(function () {
-    console.log('Hello world');
+
+    // nav menu toggle:
+    $('#menu-anchor')
+        .click(function (e) {
+            e.preventDefault();
+            var $this = $(this);
+            var target = $this.attr('href')
+            if ($this.hasClass('active')) {
+                $this.removeClass('active');
+                $(target).hide();
+            } else {
+                $this.addClass('active');
+                $(target).show();
+            }
+        })
 });
